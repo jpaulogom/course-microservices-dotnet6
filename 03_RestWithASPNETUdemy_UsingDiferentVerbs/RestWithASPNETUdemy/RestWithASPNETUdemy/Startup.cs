15b6d2d4ccee511c76a69193f9f975d615752381
jpaulogom <joaopaulo.g@outlook.com>
2023-01-02 18:42:43 -0300

    Startup complementada com extensão.

--- 03_RestWithASPNETUdemy_UsingDiferentVerbs/RestWithASPNETUdemy/RestWithASPNETUdemy/Startup.cs
+++ 03_RestWithASPNETUdemy_UsingDiferentVerbs/RestWithASPNETUdemy/RestWithASPNETUdemy/Startup.cs
@@ -1,9 +1,10 @@
 ﻿using RestWithASPNETUdemy.Services;
 using RestWithASPNETUdemy.Services.Implementations;
+using System.Runtime.CompilerServices;
 
 namespace RestWithASPNETUdemy
 {
-    public class Startup
+    public class Startup : IStartup
     {
         public Startup(IConfiguration configuration)
         {
@@ -29,6 +30,31 @@ namespace RestWithASPNETUdemy
             app.UseAuthorization();
 
             app.MapControllers();
+        }
+    }
+
+    public interface IStartup
+    {        
+        void ConfigureServices(IServiceCollection services);
+        void Configure(WebApplication app, IWebHostEnvironment env);
+    }
+
+    public static class StartupExtensions
+    {
+        public static WebApplicationBuilder UseStartup<TStartup>(this WebApplicationBuilder  WebAppBuilder) where TStartup : IStartup
+        {
+            var startup = Activator.CreateInstance(typeof(TStartup), WebAppBuilder.Configuration) as IStartup;
+
+            if (startup == null) throw new ArgumentException("Startup.cs Class is Invalid!");
+
+            startup.ConfigureServices(WebAppBuilder.Services);
+
+            var app = WebAppBuilder.Build();
+            startup.Configure(app, app.Environment);
+
+            app.Run();
+
+            return WebAppBuilder;
         }
     }
 }
