ecf8ffb829dd37afd32663ea81f23b6d0fe81f7c
jpaulogom <joaopaulo.g@outlook.com>
2023-01-05 21:20:17 -0300

    07 - Criando primeiro microsserviço

--- 03_RestWithASPNETUdemy_UsingDiferentVerbs/RestWithASPNETUdemy/RestWithASPNETUdemy/Controllers/PersonController.cs
+++ 03_RestWithASPNETUdemy_UsingDiferentVerbs/RestWithASPNETUdemy/RestWithASPNETUdemy/Controllers/PersonController.cs
@@ -1,18 +1,60 @@
 using Microsoft.AspNetCore.Mvc;
+using RestWithASPNETUdemy.Model;
+using RestWithASPNETUdemy.Services;
 
 namespace RestWithASPNETUdemy.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
+    [Route("api/[controller]")]
     public class PersonController : ControllerBase
     {
 
         private readonly ILogger<PersonController> _logger;
+        private readonly IPersonService _personService;
 
-        public PersonController(ILogger<PersonController> logger)
+        public PersonController(ILogger<PersonController> logger, IPersonService personService)
         {
             _logger = logger;
+            _personService = personService;
         }
-        
+
+        [HttpGet]
+        public ActionResult Get() 
+        {
+            return Ok(_personService.FindAll());
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult Get(long id)
+        {
+            var person = _personService.FindById(id);
+            if (person == null) return NotFound();
+
+            return Ok(person);
+        }
+
+        [HttpPost]
+        public ActionResult Post([FromBody] Person person)
+        {            
+            if (person == null) return BadRequest();
+
+            return Ok(_personService.Create(person));
+        }
+
+        [HttpPut]
+        public ActionResult Put([FromBody] Person person)
+        {
+            if (person == null) return BadRequest();
+
+            return Ok(_personService.Update(person));
+        }
+
+        [HttpDelete("{id}")]
+        public ActionResult Delete(long id)
+        {
+            _personService.Delete(id);
+            
+            return NoContent(); 
+        }
     }
 }
---
+++ 07_FirstMicrosservices/GeekShopping/GeekShopping.ProductAPI/Program.cs
@@ -0,0 +1,42 @@
+using GeekShopping.ProductAPI.Model.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace GeekShopping.ProductAPI
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            var connection = builder.Configuration["MySqlConnection:MysqlConnectionString"];
+
+            builder.Services.AddDbContext<MySQLContext>(options => options.UseMySql(
+                connection,
+                new MySqlServerVersion(new Version(8,0,31))
+                ));
+
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
